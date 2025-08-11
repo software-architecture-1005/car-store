9585b9f74e69c8acaacb88fcdc1d868b230036e6
RiveroDev <samuelenriquerivero@gmail.com>
2025-08-11 15:12:56 -0500

    Preparar views para serialización de datos y creacion de urls.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+# Esto de abajo es para importar los datos serializados y mostrarlos en la vista
+# from .serializer import CarSerializer
+# from .models import (Car o lo que sea que se vaya a importar)
 
 # Create your views here.
+#class CarView(viewsets.ModelViewSet):
+#    serializer_class = CarSerializer
+#    queryset = Car.objects.all()
