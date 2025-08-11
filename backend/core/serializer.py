d0ee7a2fd2ecc8b4e4613deebf83c471e1f4a8b8
RiveroDev <samuelenriquerivero@gmail.com>
2025-08-11 15:38:14 -0500

    Terminar de inicializar la api dejandola como comentarios

--- backend/core/serializer.py
+++ backend/core/serializer.py
@@ -2,9 +2,9 @@
 from rest_framework import serializers
 # from .models import (Cars Y Otras cositas)
 
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
+#class CarSerializer(serializers.ModelSerializer):
+#    class Meta:
         # model = Car
         # fields = (Ejmplo: 'id', 'title', 'description', 'done')
-        fields = '__all__'
+#       fields = '__all__'
 
---
+++ backend/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+# from cars import views
+
+#Versionado de la api
+#router = routers.DefaultRouter()
+#router.register(r'cars', views.CarView, 'cars')
+#
+#urlpatterns = [
+#    path("api/v1/", include(router.urls))
+#]
