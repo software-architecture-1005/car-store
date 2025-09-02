616ac03db2d635be0173d3f352352c1e33eed78c
RiveroDev <samuelenriquerivero@gmail.com>
2025-09-01 23:01:31 -0500

    Serializar modelos

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-# from cars import views
+from cars import views
 
-#Versionado de la api
-#router = routers.DefaultRouter()
-#router.register(r'cars', views.CarView, 'cars')
-#
-#urlpatterns = [
-#    path("api/v1/", include(router.urls))
-#]
+
+router = routers.DefaultRouter()
+router.register(r'cars', views.CarView, 'cars')
+
+urlpatterns = [
+    # Versionado de la api
+    path("api/v1/", include(router.urls))
+]
