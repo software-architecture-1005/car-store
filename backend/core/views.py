83320d2c082487837de9c84901dbc1dae0469c6b
RiveroDev <samuelenriquerivero@gmail.com>
2025-09-01 23:05:02 -0500

    Importar serializers y modelos en las vistas

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 # Esto de abajo es para importar los datos serializados y mostrarlos en la vista
-# from .serializer import CarSerializer
-# from .models import (Car o lo que sea que se vaya a importar)
+from .serializer import (
+    MakeSerializer, CategorySerializer, VehicleSerializer, 
+    UserSerializer, RoleSerializer, BuyerSerializer, 
+    ExpertSerializer, ReviewSerializer
+)
+from .models import Make, Category, Vehicle, User, Role, Buyer, Expert, Review
 
 # Create your views here.
 #class CarView(viewsets.ModelViewSet):
