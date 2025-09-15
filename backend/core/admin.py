1bac846c8fdae3199247ce8386bdbda2d9bef514
katals <juanca20042004@hotmail.com>
2025-09-15 18:26:34 -0500

    se añaden las tablas al admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    Role, User, Dealer, Expert, Buyer, 
+    Make, Category, Vehicle, VehicleSpecification, 
+    Review, Cart, CartItem
+)
+
 
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(Dealer)
+admin.site.register(Expert)
+admin.site.register(Buyer)
+admin.site.register(Make)
+admin.site.register(Category)
+admin.site.register(Vehicle)
+admin.site.register(VehicleSpecification)
+admin.site.register(Review)
+admin.site.register(Cart)
+admin.site.register(CartItem)
