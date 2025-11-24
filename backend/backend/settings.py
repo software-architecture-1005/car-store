0b0e9fd0e1edb64368daed753c1090fc7feb554b
RiveroDev <samuelenriquerivero@gmail.com>
2025-11-23 23:27:34 -0500

    Add STATIC_ROOT configuration for collectstatic

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3m@o934zces3xd%y!^apsqo9q*@g4+bw*e)5c8ullc+@s=aj%5'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3m@o934zces3xd%y!^apsqo9q*@g4+bw*e)5c8ullc+@s=aj%5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 
 # Application definition
@@ -139,6 +140,7 @@ USE_TZ = True  # Usa zonas horarias
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
